2eb89acfde51de48d49a2881961cc3be5edb651f
sayel <mdsayel111@gmail.com>
2024-03-17 12:52:17 +0600

    change  db url

--- idr_foundation_job_task_server/src/DB/config.js
+++ idr_foundation_job_task_server/src/DB/config.js
@@ -10,20 +10,12 @@ console.log(process.env.DB_PASS);
 //   database: "idr_foundation_job_task",
 // };
 
-const poolConfig = {
-  max: 5,
-  min: 2,
-  idleTimeoutMillis: 6000,
-};
-
 const pool = new Pool({
   connectionString:
-    "postgres://sayel111:efEU7QRc13JERy2D21x3h2mIlWuvCOsS@dpg-cnr8vqq1hbls73dufvh0-a.oregon-postgres.render.com/idr_foundation_job_task_2r9z",
+    "postgres://sayel111:efEU7QRc13JERy2D21x3h2mIlWuvCOsS@dpg-cnr8vqq1hbls73dufvh0-a/idr_foundation_job_task_2r9z",
   ssl: {
     rejectUnauthorized: false,
   },
 });
-
-// const client = new Client(config);
 
 module.exports = { pool };
